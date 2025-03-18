edaf2ffb1184992ad350b2bafa04dd9be327eb7b
Fabricio <fabricio.dvsc@outlook.com>
2025-03-18 19:25:48 -0300

    Initial commit

---
+++ src/test/java/br/com/smanager/SmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
