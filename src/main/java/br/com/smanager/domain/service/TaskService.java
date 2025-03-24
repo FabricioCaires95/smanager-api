a4742aadc42b12446c550538059402288f0fae0a
Fabricio <fabricio.dvsc@outlook.com>
2025-03-24 20:16:43 -0300

    add implementation for create a task with TaskService and TastRestResource

---
+++ src/main/java/br/com/smanager/domain/service/TaskService.java
@@ -0,0 +1,30 @@
+package br.com.smanager.domain.service;
+
+import br.com.smanager.domain.entity.Task;
+import br.com.smanager.domain.model.TaskStatus;
+import br.com.smanager.domain.repository.TaskRepository;
+import br.com.smanager.infrastructure.dto.SaveTaskDto;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Transactional
+    public Task create(SaveTaskDto taskDto) {
+        var task = Task.builder()
+                .title(taskDto.title())
+                .description(taskDto.description())
+                .numberOfDays(taskDto.numberOfDays())
+                .status(TaskStatus.PENDING)
+                .build();
+
+        return taskRepository.save(task);
+    }
+
+}
+
---
+++ src/main/java/br/com/smanager/infrastructure/controller/TaskRestResource.java
@@ -0,0 +1,58 @@
+package br.com.smanager.infrastructure.controller;
+
+import br.com.smanager.domain.service.ProjectService;
+import br.com.smanager.domain.service.TaskService;
+import br.com.smanager.infrastructure.dto.ProjectDto;
+import br.com.smanager.infrastructure.dto.SaveProjectDto;
+import br.com.smanager.infrastructure.dto.SaveTaskDto;
+import br.com.smanager.infrastructure.dto.TaskDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+import static br.com.smanager.infrastructure.controller.RestConstants.PATH_PROJECTS;
+import static br.com.smanager.infrastructure.controller.RestConstants.PATH_TASKS;
+
+@RestController
+@RequestMapping(PATH_TASKS)
+@RequiredArgsConstructor
+public class TaskRestResource {
+
+    private final TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<TaskDto> create(@RequestBody @Valid SaveTaskDto taskDto){
+
+        var task = taskService.create(taskDto);
+        return ResponseEntity.created(URI.create(PATH_PROJECTS + "/" + task.getId())).body(TaskDto.from(task));
+    }
+
+//    @GetMapping(value = "/{id}")
+//    public ResponseEntity<ProjectDto> getById(@PathVariable String id){
+//        var project = taskService.getById(id);
+//        return ResponseEntity.ok(ProjectDto.from(project));
+//    }
+//
+//    @DeleteMapping(value = "/{id}")
+//    public ResponseEntity<Void> delete(@PathVariable String id){
+//        taskService.delete(id);
+//        return ResponseEntity.noContent().build();
+//    }
+//
+//    @PutMapping(value = "/{id}")
+//    public ResponseEntity<ProjectDto> update(@PathVariable String id, @RequestBody @Valid SaveProjectDto projectDto){
+//        var updatedProject = taskService.updateProject(id, projectDto);
+//        return ResponseEntity.ok(ProjectDto.from(updatedProject));
+//    }
+
+}
